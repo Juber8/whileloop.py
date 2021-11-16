ec4dd5b953888c2309cc739e1543916dab03d0eb
Juber8 <90597119+Juber8@users.noreply.github.com>
2021-11-16 19:06:03 +0530

    problem on while loop

---
+++ while loop.py
@@ -0,0 +1,43 @@
+#write a program to print 1-10 number without using range finction
+i=1
+while (i <=10):
+    print(i)
+    i=i+1
+#take input from user
+pnint(input('enter the number'))
+i=1
+while (i<=n):
+    print(i)
+    i=i+1
+#print 10-1 value without using range function
+i=10
+while (i>0):
+    print(i)
+    i=i-1
+# find the sum of given range of user
+n=int(input('enter the last range of num'))
+sum=0
+for i in range(0,n+1):
+    sum=sum+i
+print(sum)
+n=int(input('enter the range of number'))
+sum=0
+i=1
+while (i<n+1):
+    sum=sum+i
+    i=i+1
+print(sum)
+
+s='juber'
+output=''
+i=len(s)-1
+while i>=0:
+    output=output+s[i]
+    i=i-1
+print(output)
+
+
+
+
+
+
